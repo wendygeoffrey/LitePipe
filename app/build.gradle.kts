fe0f134a844d8d7280ec05c70fe0cbd6517f2c27
wendygeoffrey <wendygeoffreyadams@gmail.com>
2026-03-26 15:54:41 +0000

    arm64 only

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
         // Only include ARM architectures for production to save space
         ndk {
-            abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a"))
+            abiFilters.addAll(listOf("arm64-v8a"))
         }
 
         // Limit resources to supported languages to reduce size
